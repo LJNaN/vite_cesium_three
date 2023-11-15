dc11dc0bca43e18b1bacca6acbed319d806bde13
柳江南 <oncwnuA5PbQxtTysj5w_BTxJ_rRU@git.weixin.qq.com>
2023-11-15 17:23:22 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+
+window.CESIUM_BASE_URL = '/Cesium/'
 import * as THREE from 'three'
-import * as CESIUM from 'cesium'
+import * as Cesium from 'cesium'
 
 console.log('THREE: ', THREE);
-console.log('CESIUM: ', CESIUM);
+console.log('Cesium: ', Cesium);
+window.THREE = THREE
+window.Cesium = Cesium
 
 
 createApp(App).mount('#app')
